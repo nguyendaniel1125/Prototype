40a35a600df0b79e02dba8055990975651c318c6
nguyendaniel1125 <nguyendaniel1125@gmail.com>
2024-11-16 21:00:07 -0800

    Update Prototype3.py

--- Prototype3.py
+++ Prototype3.py
@@ -274,5 +274,5 @@ elif option == "Flood Preparedness Advisor":
             )
             st.write(response)
   elif option == "Community Flood Reporting Map":
-        st.subheader("Community Flood Reporting Map")   
-    
+        if option == "Community Flood Reporting Map":  
+            st.subheader("Community Flood Reporting Map")
